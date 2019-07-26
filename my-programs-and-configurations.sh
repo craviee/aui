df722f03165c8f7be557d6d751b71bfe4a8e5901
craviee <danielcraviee@gmail.com>
2019-07-26 19:41:26 +0200

    installing pacaur with yay

--- my-programs-and-configurations.sh
+++ my-programs-and-configurations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Official programs
+# Installing official programs
 sudo pacman -Syu --noconfirm \
 xclip \
 deepin-music \
@@ -18,18 +18,13 @@ expac \
 yajl \
 gnupg 
 
-# Installing AUR helper
-mkdir ~/tmp/
-cd ~/tmp/
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-makepkg -i PKGBUILD --noconfirm
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
-makepkg -i PKGBUILD --noconfirm
-cd ~
-rm -r ~/tmp/
+# Installing pacaur using yay
+yay -S pacaur
 
-# AUR programs
+# Removing yay
+pacman -Rs yay
+
+# Installing AUR programs
 pacaur -S \
 brave \
 discord \
