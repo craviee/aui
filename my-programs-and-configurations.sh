63e149bd46337301e3f5c8bad46119c4ba4f36ff
craviee <danielcraviee@gmail.com>
2019-07-26 18:23:34 +0200

    script added

--- my-programs-and-configurations.sh
+++ my-programs-and-configurations.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
-# Run with sudo
 
-# Install
-#pacman -Syu --noconfirm \
-#.... \
-#.... \
+# Official programs
+sudo pacman -Syu --noconfirm \
+xclip \
+deepin-music \
+deepin-screenshot \
+termite \
+tmux \
+gimp \
+htop \
+gedit \
+libreoffice-still \
+mpv \
+ranger \
+feh \
+expac \
+yajl \
+gnupg 
+
+# Installing AUR helper
+mkdir ~/tmp/
+cd ~/tmp/
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+makepkg -i PKGBUILD --noconfirm
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg -i PKGBUILD --noconfirm
+cd ~
+rm -r ~/tmp/
+
+# AUR programs
+pacaur -S \
+brave \
+discord \
+foxitreader \
+code
