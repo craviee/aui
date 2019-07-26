7ca6124a7ff5ec3c75d29a6c52cb6f8d28d1d0d9
craviee <danielcraviee@gmail.com>
2019-07-26 21:28:27 +0200

    fixing package names and adding aliases

--- my-programs-and-configurations.sh
+++ my-programs-and-configurations.sh
@@ -16,17 +16,23 @@ ranger \
 feh \
 expac \
 yajl \
-gnupg 
+gnupg \
+scrot 
 
 # Installing pacaur using yay
 yay -S pacaur
 
 # Removing yay
-pacman -Rs yay
+sudo pacman -Rs yay
 
 # Installing AUR programs
 pacaur -S \
-brave \
+brave-bin \
 discord \
 foxitreader \
 code
+
+# Adding alias to .bashrc if it does't had yet
+grep -qxF 'alias ccc="xclip -se c"' ~/.bashrc || echo 'alias ccc="xclip -se c"' >> ~/.bashrc
+grep -qxF 'alias scrot-ccc="scrot /tmp/tmp.png && xclip -se c -target image/png -i /tmp/tmp.png"' ~/.bashrc || echo 'alias scrot-ccc="scrot /tmp/tmp.png && xclip -se c -target image/png -i /tmp/tmp.png"' >> ~/.bashrc
+grep -qxF 'alias scrot-ccc-w="scrot -u /tmp/tmp.png && xclip -se c -target image/png -i /tmp/tmp.png"' ~/.bashrc || echo 'alias scrot-ccc-w="scrot -u /tmp/tmp.png && xclip -se c -target image/png -i /tmp/tmp.png"' >> ~/.bashrc
